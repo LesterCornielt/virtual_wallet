649023c3120eee030d86935a754205582dec25a7
Lester <lestercornielt@gmail.com>
2025-06-01 13:02:42 -0400

    /fix: Three dots popup menu more accurate

--- lib/features/recipients/presentation/widgets/recipient_card.dart
+++ lib/features/recipients/presentation/widgets/recipient_card.dart
@@ -24,57 +24,70 @@ class RecipientCard extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 36.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
-                  '**** **** **** 0000',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      '**** **** **** 0000',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
-                PopupMenuButton<String>(
-                  onSelected: (value) {
-                    // Handle menu item selection (Edit or Delete)
-                    if (value == 'edit') {
-                      // Call the onEdit callback if it's not null
-                      onEdit?.call();
-                    } else if (value == 'delete') {
-                      // Call the onDelete callback if it's not null
-                      onDelete?.call();
-                    }
-                  },
-                  itemBuilder:
-                      (BuildContext context) => <PopupMenuEntry<String>>[
-                        const PopupMenuItem<String>(
-                          value: 'edit',
-                          child: ListTile(
-                            leading: Icon(Icons.edit),
-                            title: Text('Editar'),
-                          ),
-                        ),
-                        const PopupMenuItem<String>(
-                          value: 'delete',
-                          child: ListTile(
-                            leading: Icon(Icons.delete),
-                            title: Text('Eliminar'),
-                          ),
-                        ),
-                      ],
+                const SizedBox(height: 8),
+                const Text(
+                  'Nombre del Destinatario CUP',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
                 ),
               ],
             ),
-            const SizedBox(height: 8),
-            const Text(
-              'Nombre del Destinatario CUP',
-              style: TextStyle(fontSize: 16, color: Colors.white),
+            Positioned(
+              top: -22,
+              right: -10,
+              child: PopupMenuButton<String>(
+                icon: const Icon(
+                  Icons.more_horiz,
+                  color: Colors.white,
+                  size: 32,
+                ),
+                onSelected: (value) {
+                  // Handle menu item selection (Edit or Delete)
+                  if (value == 'edit') {
+                    // Call the onEdit callback if it's not null
+                    onEdit?.call();
+                  } else if (value == 'delete') {
+                    // Call the onDelete callback if it's not null
+                    onDelete?.call();
+                  }
+                },
+                itemBuilder:
+                    (BuildContext context) => <PopupMenuEntry<String>>[
+                      const PopupMenuItem<String>(
+                        value: 'edit',
+                        child: ListTile(
+                          leading: Icon(Icons.edit),
+                          title: Text('Editar'),
+                        ),
+                      ),
+                      const PopupMenuItem<String>(
+                        value: 'delete',
+                        child: ListTile(
+                          leading: Icon(Icons.delete),
+                          title: Text('Eliminar'),
+                        ),
+                      ),
+                    ],
+              ),
             ),
-            // TODO: Add bank logo if necessary
           ],
         ),
       ),
